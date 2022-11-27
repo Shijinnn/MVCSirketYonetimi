795964fa6185b4b6e82966b2f7cc67ef7a06f078
konuk <konuk@405-02>
2022-11-27 19:04:46 +0300

    Add project files.

---
+++ MVCSirketYonetimi/Controllers/CalisanController.cs
@@ -0,0 +1,76 @@
+﻿using MVCSirketYonetimi.Business.Managers;
+using MVCSirketYonetimi.Data.Models.Context;
+using MVCSirketYonetimi.Data.Models.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCSirketYonetimi.Controllers
+{
+    public class CalisanController : Controller
+    {
+        CalisanManager manager = new CalisanManager();
+        SirketDbContext db = new SirketDbContext();
+        //Http Protokolleri
+        //HtttpGet
+        //HttpPost
+        //ActionResult'lar geriye View döndürür.
+
+
+        // GET: Calisan
+        [HttpGet]
+        public ActionResult Index()
+        {
+            var calisanlar = manager.HepsiniGetir();
+            return View(calisanlar);
+        }
+
+        [HttpGet]
+        public ActionResult Ekle()
+        {
+            return View("Ekle");
+        }
+        [HttpPost]
+        public ActionResult Ekle(Calisan calisan)
+        {
+            manager.Ekle(calisan);
+            return RedirectToAction("Index", "Calisan");
+
+        }
+        [HttpGet]
+        public ActionResult Guncelle(int id)
+        {
+            var calisan = db.Calisanlar.Find(id);
+            if (calisan == null)
+            {
+                return HttpNotFound();
+            }
+            return View("Guncelle", calisan);
+
+        }
+        [HttpPost]
+        public ActionResult Guncelle(Calisan calisan)
+        {
+            manager.Guncelle(calisan);
+            return RedirectToAction("Index", "Calisan");
+
+        }
+
+
+        public ActionResult Sil(int id)
+        {
+            var calisan = db.Calisanlar.Find(id);
+            if (calisan == null)
+            {
+                return HttpNotFound();
+            }
+            manager.Sil(id);
+            return RedirectToAction("Index", "Calisan");
+
+        }
+
+
+    }
+}
---
+++ MVCSirketYonetimi/Controllers/DepartmanController.cs
@@ -0,0 +1,77 @@
+﻿using MVCSirketYonetimi.Business.Interfaces;
+using MVCSirketYonetimi.Business.Managers;
+using MVCSirketYonetimi.Data.Models.Context;
+using MVCSirketYonetimi.Data.Models.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCSirketYonetimi.Controllers
+{
+    public class DepartmanController : Controller
+    {
+        GenericDepartman
+        SirketDbContext db = new SirketDbContext();
+        //Http Protokolleri
+        //HtttpGet
+        //HttpPost
+        //ActionResult'lar geriye View döndürür.
+
+
+        // GET: Calisan
+        [HttpGet]
+        public ActionResult Index()
+        {
+            var departmanlarListesi = manager.HepsiniGetir();
+            return View(departmanlarListesi);
+        }
+
+        [HttpGet]
+        public ActionResult Ekle()
+        {
+            return View("Ekle");
+        }
+        [HttpPost]
+        public ActionResult Ekle(Departman departman)
+        {
+            manager.Ekle(departman);
+            return RedirectToAction("Index", "Departman");
+
+        }
+        [HttpGet]
+        public ActionResult Guncelle(int id)
+        {
+            var departman = db.Departmanlar.Find(id);
+            if (departman == null)
+            {
+                return HttpNotFound();
+            }
+            return View("Guncelle", departman);
+
+        }
+        [HttpPost]
+        public ActionResult Guncelle(Departman departman)
+        {
+            manager.Guncelle(departman);
+            return RedirectToAction("Index", "Departman");
+
+        }
+
+
+        public ActionResult Sil(int id)
+        {
+            var departman = db.Departmanlar.Find(id);
+            if (departman == null)
+            {
+                return HttpNotFound();
+            }
+            manager.Sil(id);
+            return RedirectToAction("Index", "Departman");
+
+        }
+
+
+    }
+}
